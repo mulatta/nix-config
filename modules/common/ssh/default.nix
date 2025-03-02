fc1fd637e3dba722958b71951fee540997aa5d50
mulatta <67085791+mulatta@users.noreply.github.com>
2025-03-03 01:53:46 +0900

    chore(ssh): fix some typo

--- modules/common/ssh/default.nix
+++ modules/common/ssh/default.nix
@@ -4,22 +4,20 @@
     matchBlocks = {
       "*" = {
         extraOptions = {
-          IgnoreUnknow = "UseKeyChain";
-          useKeyChain = "yes";
-          AddKeystoAgent = "yes";
+          IgnoreUnknown = "UseKeyChain";
+          UseKeychain = "yes";
+          AddKeysToAgent = "yes";
         };
       };
       "my-server" = {
         hostname = "100.115.94.1";
         user = "mulatta";
-        identityFile = ~/.ssh/eq12_ed25519;
+        identityFile = "~/.ssh/eq12_ed25519";
         identitiesOnly = true;
         forwardAgent = false;
       };
-      "github" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = ~/.ssh/github_ed25519;
+      "github.com" = {
+        identityFile = "~/.ssh/github_ed25519";
         identitiesOnly = true;
         forwardAgent = false;
         extraOptions = {
@@ -31,5 +29,4 @@
     };
   };
   services.ssh-agent.enable = true;
-  # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERkTnc18K6v80qc/LNfzEzQFSu1jbqA7PYuA+ch4Ui9 lsw1167@gmail.com";
 }
