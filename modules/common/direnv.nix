8f428d0a333d5798270e14621a98963b433d6462
mulatta <67085791+mulatta@users.noreply.github.com>
2025-03-05 00:58:15 +0900

    feat(modules): add direnv config

--- modules/common/direnv.nix
+++ modules/common/direnv.nix
@@ -1,10 +1,18 @@
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
-    nix-direnv.enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv = {
+      enable = true;
+      package = pkgs.nix-direnv;
+    };
     config = {
       global = {
         load_dotenv = true;
+        hide_env_diff = true;
+        log_format = "$(tput setaf 1)%e$(tput sgr0)";
       };
     };
   };
